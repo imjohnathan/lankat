1c9563f347b0692b6f6a5727bc0d2ffef3c4587f
imjohnathan <o5786486@gmail.com>
2023-12-07 15:55:05 +0800

    Feat: comfirm diaolog

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,70 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import useModalStore, { components } from "@/stores/useModalStore";
-import { Suspense } from "react";
+import { Dispatch, SetStateAction, Suspense, useState } from "react";
 import IconLoading from "~icons/line-md/loading-twotone-loop";
+
+const AlertDialogDemo = ({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const { close } = useModalStore();
+  return (
+    <AlertDialog
+      open={open}
+      onOpenChange={(open) => {
+        if (!open) {
+          setOpen(false);
+          close();
+        }
+      }}
+    >
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>注意</AlertDialogTitle>
+          <AlertDialogDescription>
+            有未儲存的變更，確定要取消編輯嗎？
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              setOpen(false);
+            }}
+            type="button"
+          >
+            返回編輯
+          </Button>
+          <Button
+            variant="destructive"
+            onClick={(e) => {
+              e.preventDefault();
+              setOpen(false);
+              close();
+            }}
+            type="button"
+          >
+            不儲存
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
 
 const Loading = () => (
   <div className="fixed inset-0 z-50 grid place-items-center bg-background/80 backdrop-blur-sm">
@@ -12,21 +73,27 @@ const Loading = () => (
 );
 
 const Modal = () => {
-  const { component, props, isLoading, isOpen, close } = useModalStore();
+  const { component, props, isLoading, isOpen, close, unSavedChanges } =
+    useModalStore();
   const ModalComponent = component ? components[component] : () => <></>;
+  const [openConfirm, setOpenConfirm] = useState(false);
   return (
-    <Suspense fallback={<Loading />}>
-      <Dialog
-        open={isOpen}
-        onOpenChange={(open) => {
-          if (!open) close();
-        }}
-      >
-        <DialogContent className={props.className + " overflow-y-auto"}>
-          {component && <ModalComponent {...props} isLoading={isLoading} />}
-        </DialogContent>
-      </Dialog>
-    </Suspense>
+    <>
+      <Suspense fallback={<Loading />}>
+        <Dialog
+          open={isOpen}
+          onOpenChange={(open) => {
+            if (!open && unSavedChanges) return setOpenConfirm(true);
+            if (!open) return close();
+          }}
+        >
+          <DialogContent className={props.className + " overflow-y-auto"}>
+            {component && <ModalComponent {...props} isLoading={isLoading} />}
+          </DialogContent>
+        </Dialog>
+        <AlertDialogDemo open={openConfirm} setOpen={setOpenConfirm} />
+      </Suspense>
+    </>
   );
 };
 
